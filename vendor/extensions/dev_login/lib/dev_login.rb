058645d9f5f94985b2bc4217c1d28344317e6461
Andrew Bloomgarden <stalkingtiger@gmail.com>
2010-05-02 16:03:51 -0700

    Cleaned up dev_login extension.

--- vendor/extensions/dev_login/lib/dev_login.rb
+++ vendor/extensions/dev_login/lib/dev_login.rb
@@ -1,17 +1,11 @@
 module DevLogin
   def self.included(base)
-    base.class_eval do
-      unless login_required?
-        prepend_before_filter :authenticate_for_dev_mode
-      end
+    base.instance_eval do
+      prepend_before_filter :authenticate_if_dev_mode
     end
   end
   
-  ##
-  # Requires authentication for any page requests in SiteController when in dev mode
-  def authenticate_for_dev_mode
-    if dev?
-      authenticate
-    end
+  def authenticate_if_dev_mode
+    authenticate if dev?
   end
 end
