702c9c5cf86a562937bd2d917d55afb92b24ae16
Andrew Bloomgarden <stalkingtiger@gmail.com>
2011-09-06 12:08:55 -0700

    Remove commented stuff that didn't work.

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,5 +1,2 @@
 Haml::Template.options[:format] = :html5
 Haml::Template.options[:ugly] = RAILS_ENV == 'production'
-
-#Compass.sass_engine_options[:load_paths].unshift "#{Rails.root}/public/stylesheets/sass"
-#raise Compass.sass_engine_options.inspect
